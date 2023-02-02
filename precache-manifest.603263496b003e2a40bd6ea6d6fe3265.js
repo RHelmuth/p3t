6b4e712605dc76e62d678431e80cb50ba5e2182d
Nick Forneris <nicholasdforneris@gmail.com>
2023-02-02 01:20:21 -0600

    Updates

---
+++ precache-manifest.603263496b003e2a40bd6ea6d6fe3265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2b43b8e90cfb49d3ee1",
+    "url": "/react-gh-pages/static/css/main.dd6e2f48.chunk.css"
+  },
+  {
+    "revision": "d2b43b8e90cfb49d3ee1",
+    "url": "/react-gh-pages/static/js/main.d2b43b8e.chunk.js"
+  },
+  {
+    "revision": "631e7dd9bb4f2490d72c",
+    "url": "/react-gh-pages/static/css/1.f569f9eb.chunk.css"
+  },
+  {
+    "revision": "631e7dd9bb4f2490d72c",
+    "url": "/react-gh-pages/static/js/1.631e7dd9.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "fe0602c06c0abd72db04d1c27a6feaa5",
+    "url": "/react-gh-pages/index.html"
+  }
+];
